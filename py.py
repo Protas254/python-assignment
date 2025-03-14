f04f133429ace3dacf9be39f01140ee6ecdbb190
Protas254 <protasjunior254@gmail.com>
2025-03-15 00:54:44 +0300

    python assignment

--- py.py
+++ py.py
@@ -1,3 +1,8 @@
+
+num_1 = int(input("Enter first value: "))
+num_2 = int(input("Enter Second value: "))
+op = input("Enter operation to perform[-,*,/, +, %]: ")
+
 if op == '+':
     result = num_1 + num_2
     print(f"{num_1} + {num_2} = {result}")
