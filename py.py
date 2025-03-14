92d9f59b27c88bc7cc50e4e55d9b9a48052fe2e8
Protas254 <protasjunior254@gmail.com>
2025-03-14 12:08:47 +0300

    python assignment

---
+++ py.py
@@ -0,0 +1,19 @@
+if op == '+':
+    result = num_1 + num_2
+    print(f"{num_1} + {num_2} = {result}")
+    
+elif  op == '-':
+    result = num_1 - num_2
+    print(f"{num_1} - {num_2} = {result}")
+
+elif op == '*':
+    result = num_1 * num_2
+    print(f"{num_1} X {num_2} = {result}")
+
+elif op == '/':
+    result = num_1 / num_2
+    print(f"{num_1} / {num_2} = {result}")
+
+else:
+    result = num_1 % num_2
+    print(f"{num_1} % {num_2} = {result}")
